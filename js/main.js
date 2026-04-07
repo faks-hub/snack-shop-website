d919380e5f2b70c77f2700b6390eb711ae786250
Fakunle Opeyemi <opeyemifakunle82@gmail.com>
2026-04-07 05:35:13 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,3 @@ function sendMail() {
     .send("service_xkb7aoa", "template_3djs6a4", parms)
     .then(alert("Email Sent!!"));
 }
-if (!parms.name || !parms.email || !parms.message) {
-  alert("Please fill in all fields!");
-  return;
-}
